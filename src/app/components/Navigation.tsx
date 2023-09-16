a5ae8f858aad40e13ae15d6ceca7b86d7d6e5612
Denis Sitdikov <denis.sitdikov95@yandex.ru>
2023-09-16 23:57:41 +0400

    fix: navigation linter mistakes

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,12 +3,12 @@ import NavigationItem from "./NavigationItem";
 const Navigation = () => {
     return (
         <ul className="w-full grid list-none justify-between sm:grid-cols-1 md:grid-cols-3max lg:grid-cols-6max">
-            <NavigationItem children="О КОМПАНИИ"/>
-            <NavigationItem children="УСЛУГИ" />
-            <NavigationItem children="КАТАЛОГ ГОТОВЫХ РЕШЕНИЙ" />
-            <NavigationItem children="ИНТЕРНЕТ-МАГАЗИН" />
-            <NavigationItem children="ДОСТАВКА И ОПЛАТА" />
-            <NavigationItem children="КОНТАКТЫ" />
+            <NavigationItem>О КОМПАНИИ</NavigationItem>
+            <NavigationItem>УСЛУГИ</NavigationItem>
+            <NavigationItem>КАТАЛОГ ГОТОВЫХ РЕШЕНИЙ</NavigationItem>
+            <NavigationItem>ИНТЕРНЕТ-МАГАЗИН</NavigationItem>
+            <NavigationItem>ДОСТАВКА И ОПЛАТА</NavigationItem>
+            <NavigationItem >КОНТАКТЫ</NavigationItem>
         </ul>
     )
 }
--- src/app/components/NavigationItem.tsx
+++ src/app/components/NavigationItem.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link"
 
 const NavigationItem = (props: {
-    children: string
-}) => {
+    children: string}) => {
     return (
         <Link href="#"><li className="inline-block mt-4 text-left whitespace-nowrap items-end transition duration-600 ease-in-out hover:brightness-50">{props.children}</li></Link>
     )
